6f56072ad0e82c7505ad7a3148f27ec80660ee5b
NelsonWang <wangnelson2@gmail.com>
2021-08-04 21:16:01 -0400

    mongoose | test

--- index.js
+++ index.js
@@ -3,12 +3,42 @@ const app = express();
 const importData = require("./data.json")
 let port = process.env.PORT || 3000;
 
+const mongoose = require("mongoose");
+
+
+mongoose.connect("mongodb+srv://nelson:1223@cluster0.kzhr5.mongodb.net/todo?retryWrites=true&w=majority", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+const User = mongoose.model("User", userSchema);
+
+const todosSchema = new mongoose.Schema({
+  userId: mongoose.Schema.ObjectId,
+  todos: [
+    {
+      checked: Boolean,
+      text: String,
+      id: String,
+    },
+  ],
+});
+const Todos = mongoose.model("Todos", todosSchema);
+
 app.get("/", (req, res) => {
     res.send("Hello World")
 });
 
 app.get("/players", (req, res) => {
     res.json(importData);
+} )
+
+app.get("/users", async (req, res) => {
+    res.json(User.find());
 } )
 
 
