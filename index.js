82fe3488f15c2db32b308ff59965a492e5dec771
NelsonWang <wangnelson2@gmail.com>
2021-08-04 23:11:48 -0400

    my api | quests | questboards API!

--- index.js
+++ index.js
@@ -1,54 +1,129 @@
-const express  = require("express");
+const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
+
 
 
 const app = express();
 const importData = require("./data.json")
 let port = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
 
 
-mongoose.connect("mongodb+srv://nelson:1223@cluster0.kzhr5.mongodb.net/todo?retryWrites=true&w=majority", {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
+mongoose.connect("mongodb+srv://nelson:1223@cluster0.kzhr5.mongodb.net/questBoard?retryWrites=true&w=majority", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
 });
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
+    username: String,
+    tags: String,
+    questsCompleted: Number,
+
 });
 const User = mongoose.model("User", userSchema);
 
-const todosSchema = new mongoose.Schema({
-  userId: mongoose.Schema.ObjectId,
-  todos: [
-    {
-      checked: Boolean,
-      text: String,
-      id: String,
-    },
-  ],
+const questSchema = new mongoose.Schema({
+    questName: String,
+    completed: Boolean,
+    description: String,
+    tags: String,
+    questBoard: Number
 });
-const Todos = mongoose.model("Todos", todosSchema);
+const Quest = mongoose.model("Quest", questSchema);
+
+
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    res.send("Welcom to Questboard API")
 });
 
-app.get("/players", (req, res) => {
-    res.json(importData);
-} )
 
 app.get("/users", async (req, res) => {
-    
-  const adqUsers = await User.findOne({"username":"wangnelson2@gmail.com"}).exec();
+
+    const adqUsers = await User.find({}).exec();
     res.send(adqUsers);
-} )
+})
+
+
+app.post("/users", async (req, res) => {
+    const {
+        username,
+        tags,
+        questsCompleted
+    } = req.body;
+    const user = await User.findOne({
+        username
+    }).exec();
+    if (user) {
+        user.tags = tags;
+        user.questsCompleted = questsCompleted;
+        res.json({
+            message: "User updated",
+            newUser: user
+        });
+    } else {
+        await User.create({
+            username,
+            tags,
+            questsCompleted
+        });
+        res.json({
+            message: "user created",
+            newUser: user
+        });
+    }
+
+});
+
+app.get("/quests", async (req, res) => {
+
+    const adqQuests = await Quest.find({}).exec();
+    res.send(adqQuests);
+});
+
+app.post("/quests", async (req, res) => {
+    const {
+        o_id,
+        questName,
+        completed,
+        description,
+        tags,
+        questBoard,
+    } = req.body;
+    const quest = await Quest.findOne({
+        o_id
+    }).exec();
+
+    if (quest) {
+        quest.questName = questName;
+        quest.completed = completed;
+        quest.description = description;
+        quest.tags = tags;
+        quest.questBoard = questBoard;
+        res.json({
+            message: "Quest updated",
+            newUser: quest
+        });
+    } else {
+        const newQuest = await Quest.create({
+            questName,
+            completed,
+            description,
+            tags,
+            questBoard,
+        });
+        res.json({
+            message: "quest created",
+            newUser: newQuest
+        });
+    }
+})
+
 
 
 app.listen(port, () => {
     console.log(`Example app is listening on port http://localhost:${port}`);
 })
-
-
-
